84e135ccad52eec3aac87fbde7e3ee56c8b6e803
John Jasa <johnjasa11@gmail.com>
2020-09-09 23:41:16 -0600

    Updated OpenFAST simulation time to 120 secs and tests accordingly

--- weis/multifidelity/test/test_blade_codes.py
+++ weis/multifidelity/test/test_blade_codes.py
@@ -33,8 +33,8 @@ class Test(unittest.TestCase):
             fname_analysis_options_openfast,
             fname_opt_options,
         )
-        np.testing.assert_allclose(wt_opt_openfast["aeroelastic.Cp_out"][0], 0.48263954745294135)
-        np.testing.assert_allclose(wt_opt_openfast["aeroelastic.P_out"][0], 7929330.5442935725)
+        np.testing.assert_allclose(wt_opt_openfast["aeroelastic.Cp_out"][0], 0.4825611931136281)
+        np.testing.assert_allclose(wt_opt_openfast["aeroelastic.P_out"][0], 7288750.70909592)
 
 if __name__ == '__main__':
     unittest.main()
