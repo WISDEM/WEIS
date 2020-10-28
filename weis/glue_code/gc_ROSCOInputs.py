bf04579e342bea01cf578bbf10830b151ecd5003
Garrett Barter <garrett.barter@nrel.gov>
2020-10-28 13:50:35 -0600

    working example

--- weis/glue_code/gc_ROSCOInputs.py
+++ weis/glue_code/gc_ROSCOInputs.py
@@ -1,19 +1,19 @@
 def assign_ROSCO_values(wt_opt, modeling_options, control):
     # ROSCO tuning parameters
-    wt_opt['tune_rosco_ivc.PC_omega']      = control['PC_omega']
-    wt_opt['tune_rosco_ivc.PC_zeta']       = control['PC_zeta']
-    wt_opt['tune_rosco_ivc.VS_omega']      = control['VS_omega']
-    wt_opt['tune_rosco_ivc.VS_zeta']       = control['VS_zeta']
+    wt_opt['tune_rosco_ivc.PC_omega']      = control['pitch']['PC_omega']
+    wt_opt['tune_rosco_ivc.PC_zeta']       = control['pitch']['PC_zeta']
+    wt_opt['tune_rosco_ivc.VS_omega']      = control['torque']['VS_omega']
+    wt_opt['tune_rosco_ivc.VS_zeta']       = control['torque']['VS_zeta']
     if modeling_options['servose']['Flp_Mode'] > 0:
         wt_opt['tune_rosco_ivc.Flp_omega']      = control['Flp_omega']
         wt_opt['tune_rosco_ivc.Flp_zeta']       = control['Flp_zeta']
     # # other optional parameters
-    wt_opt['tune_rosco_ivc.max_pitch']     = control['max_pitch']
-    wt_opt['tune_rosco_ivc.min_pitch']     = control['min_pitch']
-    wt_opt['tune_rosco_ivc.vs_minspd']     = control['minOmega']
-    wt_opt['tune_rosco_ivc.ss_vsgain']     = control['ss_vsgain']
-    wt_opt['tune_rosco_ivc.ss_pcgain']     = control['ss_pcgain']
-    wt_opt['tune_rosco_ivc.ps_percent']    = control['ps_percent']
+    wt_opt['tune_rosco_ivc.max_pitch']     = control['pitch']['max_pitch']
+    wt_opt['tune_rosco_ivc.min_pitch']     = control['pitch']['min_pitch']
+    wt_opt['tune_rosco_ivc.vs_minspd']     = control['torque']['VS_minspd']
+    wt_opt['tune_rosco_ivc.ss_vsgain']     = control['setpoint_smooth']['ss_vsgain']
+    wt_opt['tune_rosco_ivc.ss_pcgain']     = control['setpoint_smooth']['ss_pcgain']
+    wt_opt['tune_rosco_ivc.ps_percent']    = control['pitch']['ps_percent']
     # Check for proper Flp_Mode, print warning
     if modeling_options['airfoils']['n_tab'] > 1 and modeling_options['servose']['Flp_Mode'] == 0:
             print('WARNING: servose.Flp_Mode should be >= 1 for aerodynamic control.')
