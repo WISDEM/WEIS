9cffad9c33ee248151e33188846a03aa1e95b085
John Jasa <johnjasa11@gmail.com>
2020-12-15 15:10:55 -0700

    Removed defunct FusedFAST file

--- weis/glue_code/runWEIS.py
+++ weis/glue_code/runWEIS.py
@@ -198,25 +198,3 @@ def run_weis(fname_wt_input, fname_modeling_options, fname_opt_options, overridd
         return wt_opt, modeling_options, opt_options
     else:
         return [], [], []
-
-if __name__ == "__main__":
-
-    ## File management
-    run_dir = os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ) + os.sep + 'examples' + os.sep + 'rotor_opt' + os.sep
-    fname_wt_input         = run_dir + "IEA-15-240-RWT.yaml"
-    fname_modeling_options = run_dir + "modeling_options.yaml"
-    fname_analysis_options = run_dir + "analysis_options.yaml"
-
-
-    tt = time.time()
-    wt_opt, modeling_options, opt_options = run_weis(fname_wt_input, fname_modeling_options, fname_analysis_options)
-    
-    if MPI:
-        rank = MPI.COMM_WORLD.Get_rank()
-    else:
-        rank = 0
-    if rank == 0:
-        print('Run time: %f'%(time.time()-tt))
-        sys.stdout.flush()
-
-        # print(wt_opt['aeroelastic.C_miners_SC_PS'])
