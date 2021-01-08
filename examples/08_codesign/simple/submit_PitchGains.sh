41f5dfe79fc7f3010c935e441939b2b2c0b8049a
John Jasa <johnjasa11@gmail.com>
2021-01-08 14:25:46 -0700

    Adding codesign scripts

---
+++ examples/08_codesign/simple/submit_PitchGains.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=weis
+#SBATCH --time=12:00:00
+#SBATCH --job-name=job_servoopt_iea15
+#SBATCH --nodes=2             # This should be nC/36 (36 cores on eagle)
+#SBATCH --ntasks-per-node=36
+#SBATCH --mail-user john.jasa@nrel.gov
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --output=iea15.%j.out
+
+######SBATCH --partition=debug
+
+
+nDV=4 # Number of design variables (x2 for central difference)
+nOF=16  # Number of openfast runs per finite-difference evaluation
+nC=$((nDV + nDV * nOF)) # Number of cores needed. Make sure to request an appropriate number of nodes = N / 36
+
+source activate weis-env 
+
+mpirun -v -np $nC python run_optimization.py
