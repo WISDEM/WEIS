64efce4dafe9373b4d8a849a8ad70ae2288454d5
pibo <ptrbortolotti@gmail.com>
2021-01-17 18:04:29 -0700

    adapt code to new wisdem

--- weis/glue_code/gc_ROSCOInputs.py
+++ weis/glue_code/gc_ROSCOInputs.py
@@ -17,9 +17,9 @@ def assign_ROSCO_values(wt_opt, modeling_options, control):
     wt_opt['tune_rosco_ivc.ss_pcgain']     = control['setpoint_smooth']['ss_pcgain']
     wt_opt['tune_rosco_ivc.ps_percent']    = control['pitch']['ps_percent']
     # Check for proper Flp_Mode, print warning
-    if modeling_options['RotorSE']['n_tab'] > 1 and modeling_options['Level3']['ROSCO']['Flp_Mode'] == 0:
+    if modeling_options['WISDEM']['RotorSE']['n_tab'] > 1 and modeling_options['Level3']['ROSCO']['Flp_Mode'] == 0:
             raise Exception('A distributed aerodynamic control device is specified in the geometry yaml, but Flp_Mode is zero in the modeling options.')
-    if modeling_options['RotorSE']['n_tab'] == 1 and modeling_options['Level3']['ROSCO']['Flp_Mode'] > 0:
+    if modeling_options['WISDEM']['RotorSE']['n_tab'] == 1 and modeling_options['Level3']['ROSCO']['Flp_Mode'] > 0:
             raise Exception('Flp_Mode is non zero in the modeling options, but no distributed aerodynamic control device is specified in the geometry yaml.')
 
     return wt_opt
