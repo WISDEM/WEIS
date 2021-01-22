13312d433a8de63aac53f6426170382f98eadcfd
John Jasa <johnjasa11@gmail.com>
2021-01-22 13:12:36 -0700

    Minor updates to bash scripts

--- examples/08_codesign/codesign/submit_PitchGains.sh
+++ examples/08_codesign/codesign/submit_PitchGains.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --account=weis
-#SBATCH --time=18:00:00
-#SBATCH --job-name=control_and_tower
+#SBATCH --time=1:00:00
+#SBATCH --job-name=codesign
 #SBATCH --nodes=1             # This should be nC/36 (36 cores on eagle)
 #SBATCH --ntasks-per-node=36
 #SBATCH --mail-user john.jasa@nrel.gov
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --output=iea15.%j.out
-#####SBATCH --partition=debug
+#SBATCH --partition=debug
 
 
-nDV=5 # Number of design variables (x2 for central difference)
-nOF=6  # Number of openfast runs per finite-difference evaluation
+nDV=4 # Number of design variables (x2 for central difference)
+nOF=1  # Number of openfast runs per finite-difference evaluation
 nC=$((nDV + nDV * nOF)) # Number of cores needed. Make sure to request an appropriate number of nodes = N / 36
 
 source activate weis-env 
