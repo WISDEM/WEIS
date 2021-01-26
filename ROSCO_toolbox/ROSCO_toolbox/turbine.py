e278441f3a99e67dd99792d3075dadd7da680455
Nikhar Abbas <nikhar.abbas@colorado.edu>
2021-01-26 13:10:45 -0700

    Revert "Squashed 'ROSCO_toolbox/' changes from 096f1e9a..71565e1a"
    This reverts commit 85a710c66013df5a187140d4bfea145dd1e80cde.

--- ROSCO_toolbox/ROSCO_toolbox/turbine.py
+++ ROSCO_toolbox/ROSCO_toolbox/turbine.py
@@ -545,7 +545,7 @@ class Turbine():
         self.span = r 
         self.chord = chord
         self.twist = theta
-        self.bld_flapwise_damp = self.fast.fst_vt['ElastoDynBlade']['BldFlDmp1']/100
+        self.bld_flapwise_damp = self.fast.fst_vt['ElastoDynBlade']['BldFlDmp1']/100 * 0.7
 
 class RotorPerformance():
     '''
