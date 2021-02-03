4e9f675ce2129846dc144b5cbc703128cbe6e8b2
Garrett Barter <garrett.barter@nrel.gov>
2021-02-03 14:14:45 -0700

    printing out comparison values

--- weis/test/utils.py
+++ weis/test/utils.py
@@ -76,9 +76,9 @@ def compare_regression_values(
                     continue
                 testing_value = output_dict[key]
                 truth_value = truth_dict[key]
-
+                print(key, testing_value, truth_value)
                 try:
-                    assert_near_equal(output_dict[key], truth_dict[key], tolerance=tol)
+                    assert_near_equal(testing_value, truth_value, tolerance=tol)
                 except Exception as e:
                     print()
                     print(f"Error: Comparison values not equal for {key}.")
