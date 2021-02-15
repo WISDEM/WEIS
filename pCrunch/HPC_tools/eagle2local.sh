0062bdd73db0b7fc21fcad0b61843e2d4988d52b
dzalkind <dzalkind@nrel.gov>
2021-02-15 14:40:01 -0700

    Update eagle rsync script

--- pCrunch/HPC_tools/eagle2local.sh
+++ pCrunch/HPC_tools/eagle2local.sh
@@ -1,10 +1,20 @@
 # ------ Sync DLC Data from eagle runs locally -------
 
 # --- 5MW LAND LEGACY ---
-outdir='/home/dzalkind/Tools/WEIS-4/examples/03_NREL5MW_OC3_spar/outputs_pitch_control/'
-indir='/Users/dzalkind/Tools/WEIS-4/examples/03_NREL5MW_OC3_spar/outputs_pitch_control/'
+
+
+# SQL Files
+# outdir='/scratch/dzalkind/WEIS-4/optimizations/outputs_opt_ball_step10/'
+# indir='/Users/dzalkind/Tools/WEIS-4/optimizations/outputs_opt_ball_step10/'
+# mkdir -p $indir;
+# rsync -aP --no-g --include="*/" --include="log_opt.sql*" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+
+# FAST Outs
+outdir='/scratch/dzalkind/WEIS-4/optimizations/outputs_opt_pitch_5en3/'
+indir='/Users/dzalkind/Tools/WEIS-4/optimizations/outputs_opt_pitch_5en3/'
 mkdir -p $indir;
-rsync -aP --no-g --include="*/" --include="log_opt.sql*" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+rsync -aP --no-g --include="*/" --include="NREL5MW_OC3_spar_*_IEC_0.out" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
 
 # rsync dzalkind@eagle.hpc.nrel.gov:$outdir/case_matrix.yaml $indir
 
