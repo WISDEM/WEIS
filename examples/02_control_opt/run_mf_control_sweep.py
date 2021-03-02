85e01e0e4ab37ed41306b3e6fe3c624720dc9726
dzalkind <dzalkind@nrel.gov>
2021-03-02 12:06:45 -0700

    Add PtfmPitch_Std and tune controller

--- examples/02_control_opt/run_mf_control_sweep.py
+++ examples/02_control_opt/run_mf_control_sweep.py
@@ -119,6 +119,7 @@ class MF_Turbine(object):
         self.LinearTurbine = lin_mod.LinearTurbineModel(mf_turb.FAST_level2_Directory,reduceStates=False)
 
     def run_level2(self,controller,disturbance):
+        controller.tune_controller(self.turbine)
         linCont             = lin_mod.LinearControlModel(controller)
         self.level2_out     = []
         for dist in disturbance:
@@ -361,11 +362,15 @@ def compute_outputs(levelX_out):
     GenSpeed_Max = [lx_out['GenSpeed'].max() for lx_out in levelX_out]
     GenSpeed_Std = [lx_out['GenSpeed'].std() for lx_out in levelX_out]
 
+    # Platform pitch measures
+    PtfmPitch_Std = [lx_out['PtfmPitch'].std() for lx_out in levelX_out]
+
     # save outputs
     outputs = {}
     outputs['TwrBsMyt_DEL']     = TwrBsMyt_DEL[0]
     outputs['GenSpeed_Max']     = GenSpeed_Max[0]
     outputs['GenSpeed_Std']     = GenSpeed_Std[0]
+    outputs['PtfmPitch_Std']    = PtfmPitch_Std[0]
 
     return outputs
 
