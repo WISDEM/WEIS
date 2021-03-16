3844273a2d463c0bed267cf6d17749964c73d3dd
pibo <ptrbortolotti@gmail.com>
2021-03-16 14:00:47 -0600

    fix bug, now stats only from TStart

--- weis/aeroelasticse/runFAST_pywrapper.py
+++ weis/aeroelasticse/runFAST_pywrapper.py
@@ -170,7 +170,7 @@ class runFAST_pywrapper(object):
         for i, channel in enumerate(openfastlib.output_channel_names):
             output_dict[channel] = openfastlib.output_values[:,i]
 
-        output = OpenFASTOutput.from_dict(output_dict, self.FAST_namingOut, magnitude_channels=magnitude_channels)
+        output = OpenFASTOutput.from_dict(output_dict, self.FAST_namingOut, magnitude_channels=magnitude_channels, trim_data=(self.fst_vt['Fst']['TStart'],self.fst_vt['Fst']['TMax']))
         case_name, sum_stats, extremes, dels = la._process_output(output)
 
         # if save_file: write_fast
