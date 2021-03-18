0d58ae303f2344cce21d2d481b181d5aa85a51a0
Garrett Barter <garrett.barter@nrel.gov>
2021-03-18 07:53:51 -0600

    comment out changed line

--- weis/aeroelasticse/runFAST_pywrapper.py
+++ weis/aeroelasticse/runFAST_pywrapper.py
@@ -171,7 +171,8 @@ class runFAST_pywrapper(object):
             output_dict[channel] = openfastlib.output_values[:,i]
 
         output = OpenFASTOutput.from_dict(output_dict, self.FAST_namingOut, magnitude_channels=magnitude_channels)
-        output.trim_data(tmin = self.fst_vt['Fst']['TStart'], tmax=self.fst_vt['Fst']['TMax'])
+        print('TRIMMING:',self.fst_vt['Fst']['TStart'], self.fst_vt['Fst']['TMax'])
+        #output.trim_data(tmin = self.fst_vt['Fst']['TStart'], tmax=self.fst_vt['Fst']['TMax'])
         case_name, sum_stats, extremes, dels = la._process_output(output)
 
         # if save_file: write_fast
