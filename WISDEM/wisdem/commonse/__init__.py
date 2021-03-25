843208bb389b77dd1d89b868ccd2b36f36645349
John Jasa <johnjasa11@gmail.com>
2021-03-25 14:30:49 -0600

    Removed WISDEM

--- WISDEM/wisdem/commonse/__init__.py
+++
@@ -1,6 +0,0 @@
-from wisdem.commonse.utilities import cosd, sind, tand
-from wisdem.commonse.csystem import DirectionVector
-from wisdem.commonse.akima import Akima
-from wisdem.commonse.constants import *
-
-NFREQ = 6
