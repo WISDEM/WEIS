783211c142ce4412c7663cc4b0a62b2bbf7cf284
pibo <ptrbortolotti@gmail.com>
2021-05-24 21:32:23 -0600

    improve a few things to run olaf from weis

--- weis/aeroelasticse/openmdao_openfast.py
+++ weis/aeroelasticse/openmdao_openfast.py
@@ -1994,36 +1994,49 @@ class FASTLoadCases(ExplicitComponent):
 def RayleighCDF(x, xbar=10.):
     return 1.0 - np.exp(-np.pi/4.0*(x/xbar)**2)
 
-def OLAFParams(omega_rpm, deltaPsiDeg=6, nNWrot=2, nFWrot=10, nFWrotFree=3, nPerAzimuth=None):
+def OLAFParams(omega_rpm, deltaPsiDeg=6, nNWrot=2, nFWrot=10, nFWrotFree=3, nPerRot=None, totalRot=None, show=False):
     """
     Computes recommended time step and wake length based on the rotational speed in RPM
-
+ 
     INPUTS:
      - omega_rpm: rotational speed in RPM
      - deltaPsiDeg : azimuthal discretization in deg
      - nNWrot : number of near wake rotations
      - nFWrot : total number of far wake rotations
      - nFWrotFree : number of far wake rotations that are free
-
-        deltaPsiDeg  -  nPerAzimuth
-             5            72
-             6            60
-             7            51.5
-             8            45
+ 
+        deltaPsiDeg  -  nPerRot
+             5            72   
+             6            60   
+             7            51.5 
+             8            45   
     """
+    omega_rpm = np.asarray(omega_rpm)
     omega = omega_rpm*2*np.pi/60
     T = 2*np.pi/omega
-    if nPerAzimuth is not None:
-        dt_wanted    = np.around(T/nPerAzimuth,2)
+    if nPerRot is not None:
+        dt_wanted    = np.around(T/nPerRot,4)
     else:
-        dt_wanted    = np.around(deltaPsiDeg/(6*omega_rpm), 4)
-        nPerAzimuth = int(2*np.pi /(deltaPsiDeg*np.pi/180))
-
-    nNWPanel     = nNWrot*nPerAzimuth
-    nFWPanel     = nFWrot*nPerAzimuth
-    nFWPanelFree = nFWrotFree*nPerAzimuth
-
-    return dt_wanted, nNWPanel, nFWPanel, nFWPanelFree
+        dt_wanted    = np.around(deltaPsiDeg/(6*omega_rpm),4)
+        nPerRot = int(2*np.pi /(deltaPsiDeg*np.pi/180))
+ 
+    nNWPanel     = nNWrot*nPerRot
+    nFWPanel     = nFWrot*nPerRot
+    nFWPanelFree = nFWrotFree*nPerRot
+ 
+    if totalRot is None:
+        totalRot = (nNWrot + nFWrot)*3 # going three-times through the entire wake
+ 
+    tMax = dt_wanted*nPerRot*totalRot
+ 
+    if show:
+        print(dt_wanted              , '  dt')
+        print(int      (nNWPanel    ), '  nNWPanel          ({} rotations)'.format(nNWrot))
+        print(int      (nFWPanel    ), '  FarWakeLength     ({} rotations)'.format(nFWrot))
+        print(int      (nFWPanelFree), '  FreeFarWakeLength ({} rotations)'.format(nFWrotFree))
+        print(tMax              , '  Tmax ({} rotations)'.format(totalRot))
+ 
+    return dt_wanted, tMax, nNWPanel, nFWPanel, nFWPanelFree
 
 class ModesElastoDyn(ExplicitComponent):
     """
