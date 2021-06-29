5306b56112c9006eaf74c98da814856fcc29d4b3
dzalkind <dzalkind@nrel.gov>
2021-06-29 17:40:02 -0600

    Use WEIS-based IEA-15MW semi

--- examples/12_linearization/doe_driver.py
+++ examples/12_linearization/doe_driver.py
@@ -1,12 +1,9 @@
-import os, time, sys
+import os
 from weis.glue_code.runWEIS     import run_weis
 
-
-## File management
-run_dir                = os.path.dirname( os.path.realpath(__file__) ) + os.sep
-wisdem_examples        = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
-fname_wt_input         = os.path.join(wisdem_examples, "09_floating", "IEA-15-240-RWT_VolturnUS-S.yaml")
-fname_modeling_options = run_dir + os.sep + "modeling_options_doe.yaml"
-fname_analysis_options = run_dir + os.sep + "analysis_options_doe.yaml"
+mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
+fname_wt_input         = mydir + os.sep + "IEA-15-floating.yaml"
+fname_modeling_options = mydir + os.sep + "modeling_options_doe.yaml"
+fname_analysis_options = mydir + os.sep + "analysis_options_doe.yaml"
 
 wt_opt, modeling_options, analysis_options = run_weis(fname_wt_input, fname_modeling_options, fname_analysis_options)
