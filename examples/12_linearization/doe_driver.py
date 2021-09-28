101c5f650adc35f7ccd2dbf428de664925da7a35
dzalkind <dzalkind@nrel.gov>
2021-09-28 17:20:28 -0600

    Point linearization examples to wisdem geometry yaml

--- examples/12_linearization/doe_driver.py
+++ examples/12_linearization/doe_driver.py
@@ -2,7 +2,8 @@ import os
 from weis.glue_code.runWEIS     import run_weis
 
 mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
-fname_wt_input         = mydir + os.sep + "IEA-15-floating.yaml"
+wisdem_examples        = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
+fname_wt_input         = os.path.join(wisdem_examples, "09_floating", "IEA-15-240-RWT_VolturnUS-S.yaml")
 fname_modeling_options = mydir + os.sep + "modeling_options_doe.yaml"
 fname_analysis_options = mydir + os.sep + "analysis_options_doe.yaml"
 
