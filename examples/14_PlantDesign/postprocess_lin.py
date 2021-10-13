164275593ffd8b0b7d3685dd06296c3ebb114f0b
AthulKrishnaSundarrajan <rajan.s.athul@gmail.com>
2021-10-13 16:14:38 -0600

    New linearizations

---
+++ examples/14_PlantDesign/postprocess_lin.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import openmdao.api as om
+import pickle
+
+
+mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
+weis_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+opt_path = mydir + os.sep + "outputs" + os.sep + "IEA_level2" 
+pkl_path = opt_path + os.sep+  "ABCD_matrices.pkl"
+
+with open(pkl_path, 'rb') as handle:
+
+    ABCD_list = pickle.load(handle)
+  
+print("Information available in the pickle file:")
+for key in ABCD_list[0]:
+    print(key)
+print()    
+
+
+cr = om.CaseReader(opt_path+ os.sep +"log_opt.sql")
+
+driver_cases = cr.get_cases('driver')
+
+A_plot = []
+DVs = []
+for idx, case in enumerate(driver_cases):
+    print('===================')
+    print('Simulation index:', ABCD_list[idx]['sim_idx'])
+    dvs = case.get_design_vars(scaled=False)
+    for key in dvs.keys():
+        print(key)
+        print(dvs[key])
+    print()
+    print("A matrix")
+    #print(ABCD_list[idx]['A'])
+    print()
+    
+    A_plot.append(ABCD_list[idx]['A'][1, 1])
+    DVs.append(dvs[key])
+    
+# import matplotlib.pyplot as plt
+# 
+# A_plot = np.array(A_plot)
+# DVs = np.array(DVs)
+# 
+# plt.scatter(DVs, A_plot[:])
+# 
+# plt.xlabel("Tower Young's Modulus, Pa")
+# plt.ylabel('A[1, 1]')
+# plt.tight_layout()
+# 
+# plt.show()
