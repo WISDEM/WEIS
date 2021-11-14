6f378c86fc03cec436da9ac8ec2ad453baf06a7e
Garrett Barter <garrett.barter@nrel.gov>
2021-11-14 16:25:16 -0700

    Squashed 'MoorPy/' changes from 0d7126f15..4d6bd4f94
    4d6bd4f94 Minor: line colors and widths, distributed tensions and positions:
    36bd42b47 New documentation settings to try to get it to work
    a9f80d88b Removing autodocs for now
    5bf68abdc A first pass at documentation
    98f8bb284 Adding examples folder with 2 basic demos.
    a596531ea Bottom-up improvement of line stiffness terms:
    495e8136b Renaming solveEquilibrium3() to solveEquilibrium()
    d8f2ac737 Cleaning up local and remote repo, sE3 bug in getCoupledStiffness
    6accf8b92 Adjustments to account for MoorDyn 'seabed' feature and LBotDyn
    e27647c31 Fixed one catenary end A-B stiffness mix up while working toward new stiffness handling.
    2194f6151 Work-in-progress for better catenary stiffness matrix
    b612fffca System equilibrium solve animation now working
    564f570a9 Implemented "colortension" feature as plotting option
    8ebe8e27c Made a System have option to be qs; More additions to plot and plot2d
    e88816ca2 System.plot() fix up with bathymetry plotting capability
    ff1a4bffe Added animatelines functionality to System.animatelines()
    5c4fae5cd Plotting and large-scale solving tweaks:
    602551826 Adding option for SolveEquilibrium to not raise error if unconverged:
    46b3d0065 Reducing redundant tension code, rotation fix, Line.getCoordinate:
    4f9164b69 Added node coordinates to drawLine2D...not really necessary
    ed1ab7777 Adding a scale parameter to System.transform:
    09926f209 A small catenary tweak for near-vertical lines with sag - more appropriate solver retry.
    8d6c798a8 MoorPy load/unload section name updates and transform bugfix:
    96901eca7 Bugfix:Adding escape in solveEquilibrium3 when there are 0 DOF.
    5e518fe21 Catenary bugfix and solveEquilibrium improvements:
    c191e9b99 Adding tension jacobian and enlarging solveEq max step size:
    c17a07ab2 Merge in Ericka's plotting improvements from https://github.com/erickaloz/moorpy dev
    a3d9a96a3 Merge catenary overhaul from 'CatenaryV2' into dev
    96e90baf0 Swap finite difference stiffness matrix rows-columns
    57c967086 Some corrections for catenary slack cases
    a1aac2233 Catenary plot restructure and seabed line improvements:
    0d531df97 Tol to tol syntax bug
    b7484070e Finishing catenary v2 updates and integrating in MoorPy:
    102f3f1b8 Adding ytol to dsolve to support position-based convergence threshold in catenary
    8e16ad7ad Merge branch 'NREL:dev' into dev
    3b119eb26 completed basic U-seabed functionality and switch to stiffness matrices in catenary
    d59621fe5 Many Catenary changes in progress for U seabed contact etc:
    76772f780 added color tension plot example
    968da88cb added end points and color tension plot
    26d9b796b Added capability to rotate/transform rPointRel's on bodies
    9191d1888 a couple test files
    7c873ebfb catenary changes in progress
    9d2322e40 Merge pull request #2 from erickaloz/dev
    fa7e575b2 added line/node labeling
    0317303c8 adds on line numbers to plot function
    de00e5653 Quick fixes to get MoorPy to load the 4-turbine array for FAST.Farm
    9e4178022 Added tests folder and a first set of tests for catenary.
    f0bc4016b Simplifying catenary code, combining ProfileType 2 and 3:
    5407c238b Swapping catenary ProfileType 0 and 4 definitions to be more intuitive.
    9bd6c3c6b Slight console output tweaks related to solving equilibrium.
    207d6f6e1 Fixes to getCoupledStiffness for better convergence check
    29e590989 Updates to better support buoyant lines:
    67b5863ba Overdue commit: mostly System.unload() amendments
    REVERT: 0d7126f15 Merge branch 'dev' of git://github.com/nrel/moorpy
    REVERT: 1d26a363c removing more breakpoints
    REVERT: 7bb9b071e remove debugging flag
    REVERT: f7c985beb Merge branch 'dev' of git://github.com/nrel/moorpy
    REVERT: 0798f2999 Merge branch 'dev' of git://github.com/nrel/moorpy
    REVERT: 6142ccc2c fixing numpy 1.20 warnings and making plot imports a graceful fail
    
    git-subtree-dir: MoorPy
    git-subtree-split: 4d6bd4f949699b0ee62b6035ced45c5f1a03d73d

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../moorpy'))
 
 import moorpy
 
@@ -32,7 +32,10 @@ release = '0.9.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.coverage',
+              'sphinx.ext.napoleon',
+              "sphinx.ext.autosectionlabel",
 ]
 
 napoleon_include_init_with_doc = True
@@ -54,9 +57,7 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 #html_theme = 'default'
 html_theme = 'sphinx_rtd_theme' # need to run pip install sphinx_rtd_theme to use locally
-html_theme_options = {
-    'style_nav_header_background': 'slateblue'
-}
+#html_theme_options = {'style_nav_header_background': 'slateblue'}
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
