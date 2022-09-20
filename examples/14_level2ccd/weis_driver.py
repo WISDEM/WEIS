ab5def9387f25a291087e77a229ca21f4b184053
AthulKrishnaSundarrajan <rajan.s.athul@gmail.com>
2022-09-20 10:26:31 -0600

    12p1 cleanup

--- examples/14_level2ccd/weis_driver.py
+++ examples/14_level2ccd/weis_driver.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
     
     # get path to modelling, geometry and analysis files
-    fname_modeling_options = mydir + os.sep + "modeling_options.yaml"
     run_dir                = os.path.dirname( os.path.realpath(__file__) ) + os.sep
     wisdem_examples        = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
     fname_wt_input         = os.path.join(wisdem_examples,'09_floating/IEA-15-240-RWT_VolturnUS-S.yaml')
-    fname_analysis_options      = mydir + os.sep + "analysis_options.yaml"
+    fname_analysis_options = mydir + os.sep + "analysis_options.yaml"
+    fname_modeling_options = mydir + os.sep + "modeling_options.yaml"
     
     wt_opt, modeling_options, opt_options = run_weis(fname_wt_input, fname_modeling_options, fname_analysis_options)
     
