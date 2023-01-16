8ef456d04b3008a755636423ec45c0c7af8fc710
Garrett Barter <garrett.barter@nrel.gov>
2023-01-15 19:10:42 -0700

    Squashed 'pyHAMS/' changes from 92403cbb4..02c01bed9
    02c01bed9 Merge branch 'meson_with_test'
    70a3169e2 improving robustness with new builds
    e3bfb5ad4 Merge pull request #4 from WISDEM/meson_with_test
    b9391db89 Merge branch 'main' into meson_with_test
    8b7a93d42 Merge pull request #3 from WISDEM/more_test
    541ab413f sync with HAMS for ifx support too
    94b40a982 trying to be more robust
    b4d534377 trying to be more robust
    8a9bcc2d0 try lapack openblas again
    1fd353eec switch to openblas
    74969c39e try again
    501c5f88c better testing
    98f1310b8 reverting chardet, adding lapack
    9141ae911 get a good compiler on windows
    8c2f8dbac removed non ascii characters
    c3cb8fec3 trying with chardet
    d95974b48 probably don't need this anymore
    fe0dd0372 now working with meson (i think)
    3a789bc11 Merge branch 'more_test' into meson_with_test
    192e8d0cd now with truth values
    480a4d91f remove double negative signs
    064a5efc5 add test command
    06f6ef8af better tests
    d1858793a add the toml-file
    541b138b6 need pyproject toml-file
    af701c9ef copy over mod files?
    abbddf7e4 first start of updated build and improved test
    eaa3d4ccf Update README.md
    
    git-subtree-dir: pyHAMS
    git-subtree-split: 02c01bed9d9569045d036913bb9f7dcd38c2ff54

--- setup.py
+++ setup.py
@@ -1,83 +1,187 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import setuptools #import setup
-from numpy.distutils.core import setup, Extension
+# setup.py
+# only if building in place: ``python setup.py build_ext --inplace``
 import os
+import re
 import platform
-import sys
-import sysconfig
+import shutil
+import setuptools
+import subprocess
 
-os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
+
+def run_meson_build(staging_dir):
+    prefix = os.path.join(os.getcwd(), staging_dir)
+    purelibdir = "."
+
+    # check if meson extra args are specified
+    meson_args = ""
+    if "MESON_ARGS" in os.environ:
+        meson_args = os.environ["MESON_ARGS"]
+
+    if platform.system() == "Windows":
+        if not "FC" in os.environ:
+            os.environ["FC"] = "gfortran"
+        if not "CC" in os.environ:
+            os.environ["CC"] = "gcc"
+
+    # configure
+    meson_path = shutil.which("meson")
+    meson_call = (
+        f"{meson_path} setup {staging_dir} --prefix={prefix} "
+        + f"-Dpython.purelibdir={purelibdir} -Dpython.platlibdir={purelibdir} {meson_args}"
+    )
+    sysargs = meson_call.split(" ")
+    sysargs = [arg for arg in sysargs if arg != ""]
+    print(sysargs)
+    p1 = subprocess.run(sysargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    os.makedirs(staging_dir, exist_ok=True)
+    setup_log = os.path.join(staging_dir, "setup.log")
+    with open(setup_log, "wb") as f:
+        f.write(p1.stdout)
+    if p1.returncode != 0:
+        with open(setup_log, "r") as f:
+            print(f.read())
+        raise OSError(sysargs, f"The meson setup command failed! Check the log at {setup_log} for more information.")
+
+    # build
+    meson_call = f"{meson_path} compile -vC {staging_dir}"
+    sysargs = meson_call.split(" ")
+    sysargs = [arg for arg in sysargs if arg != ""]
+    print(sysargs)
+    p2 = subprocess.run(sysargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    compile_log = os.path.join(staging_dir, "compile.log")
+    with open(compile_log, "wb") as f:
+        f.write(p2.stdout)
+    if p2.returncode != 0:
+        with open(compile_log, "r") as f:
+            print(f.read())
+        raise OSError(
+            sysargs, f"The meson compile command failed! Check the log at {compile_log} for more information."
+        )
+
+
+def copy_shared_libraries():
+    build_path = os.path.join(staging_dir, "pyhams")
+    for root, _dirs, files in os.walk(build_path):
+        for file in files:
+            # move pyhams to just under staging_dir
+            if file.endswith((".so", ".lib", ".pyd", ".pdb", ".dylib", ".dll", ".mod")):
+                if ".so.p" in root or ".pyd.p" in root:  # excludes intermediate object files
+                    continue
+                file_path = os.path.join(root, file)
+                new_path = str(file_path)
+                match = re.search(staging_dir, new_path)
+                new_path = new_path[match.span()[1] + 1 :]
+                print(f"Copying build file {file_path} -> {new_path}")
+                shutil.copy(file_path, new_path)
+
+
+if __name__ == "__main__":
+    # This is where the meson build system will install to, it is then
+    # used as the sources for setuptools
+    staging_dir = "meson_build"
+
+    # If on Windows, use the compiled exe from Yingyi
+    if ( (not platform.system() == "Windows") and
+         ("dist" not in str(os.path.abspath(__file__))) ):
+        cwd = os.getcwd()
+        run_meson_build(staging_dir)
+        os.chdir(cwd)
+        copy_shared_libraries()
+
+    #docs_require = ""
+    #req_txt = os.path.join("doc", "requirements.txt")
+    #if os.path.isfile(req_txt):
+    #    with open(req_txt) as f:
+    #        docs_require = f.read().splitlines()
+
+    init_file = os.path.join("pyhams", "__init__.py")
+    #__version__ = re.findall(
+    #    r"""__version__ = ["']+([0-9\.]*)["']+""",
+    #    open(init_file).read(),
+    #)[0]
+
+    setuptools.setup(
+        name='pyHAMS',
+        version='1.0.0',
+        description='Python module wrapping around HAMS',
+        long_description="pyHAM is a Python interface to the HAMS boundary-element solver for underwater potential flow solutions",
+        author='NREL WISDEM Team',
+        author_email='systems.engineering@nrel.gov',
+        license='Apache License, Version 2.0',
+        install_requires=[
+            "numpy",
+            "meson",
+            "ninja",
+        ],
+        extras_require={
+            "testing": ["pytest"],
+        },
+        python_requires=">=3.8",
+        package_data={"": ["*.yaml", "*.so", "*.lib", "*.pyd", "*.pdb", "*.dylib", "*.dll"]},
+        packages=['pyhams'],
+        zip_safe=False,
+    )
+
+#os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 
 # Source order is important for dependencies
-f90src = ['WavDynMods.f90',
-          'PatclVelct.f90',
-          'BodyIntgr.f90',
-          'BodyIntgr_irr.f90',
-          'AssbMatx.f90',    
-          'AssbMatx_irr.f90',
-          'SingularIntgr.f90',
-          'InfGreen_Appr.f90',
-          'FinGrnExtSubs.f90',
-          'FinGreen3D.f90',
-          'CalGreenFunc.f90',
-          'HydroStatic.f90',
-          'ImplementSubs.f90',
-          'InputFiles.f90',
-          'NormalProcess.f90',
-          'ReadPanelMesh.f90',
-          'PotentWavForce.f90',
-          'PressureElevation.f90',
-          'PrintOutput.f90',
-          'SolveMotion.f90',
-          'WavDynSubs.f90',
-          'HAMS_Prog.f90',
-          'HAMS_Prog.pyf',
-          ]
-root_dir = os.path.join('pyhams','src')
+#f90src = ['WavDynMods.f90',
+#          'PatclVelct.f90',
+#          'BodyIntgr.f90',
+#          'BodyIntgr_irr.f90',
+#          'AssbMatx.f90',    
+#          'AssbMatx_irr.f90',
+#          'SingularIntgr.f90',
+#          'InfGreen_Appr.f90',
+#          'FinGrnExtSubs.f90',
+#          'FinGreen3D.f90',
+#          'CalGreenFunc.f90',
+#          'HydroStatic.f90',
+#          'ImplementSubs.f90',
+#          'InputFiles.f90',
+#          'NormalProcess.f90',
+#          'ReadPanelMesh.f90',
+#          'PotentWavForce.f90',
+#          'PressureElevation.f90',
+#          'PrintOutput.f90',
+#          'SolveMotion.f90',
+#          'WavDynSubs.f90',
+#          'HAMS_Prog.f90',
+#          'HAMS_Prog.pyf',
+#          ]
+#root_dir = os.path.join('pyhams','src')
 
-intel_flag = sysconfig.get_config_var('FC') == 'ifort'
+#intel_flag = sysconfig.get_config_var('FC') == 'ifort'
 
-if not intel_flag:
-    for a in sys.argv:
-        intel_flag = intel_flag or a.find('intel')>=0
+#if not intel_flag:
+#    for a in sys.argv:
+#        intel_flag = intel_flag or a.find('intel')>=0
 
-if not intel_flag:
-    try:
-        if os.environ['FC'] == 'ifort':
-            intel_flag = True
-    except KeyError:
-        pass
+#if not intel_flag:
+#    try:
+#        if os.environ['FC'] == 'ifort':
+#            intel_flag = True
+#    except KeyError:
+#        pass
 
-myargs = ['-O3','-m64','-fPIC','-g']
-mycargs = ['-std=c11']
-if intel_flag:
-    myfargs = ['-mkl']
-    mylib = ['mkl_rt']
-    mylink = []
-else:
-    myfargs = ['-fno-align-commons','-fdec-math']
-    mylib = ['lapack']
-    mylink = []
-
-pyhamsExt = Extension('pyhams.libhams', sources=[os.path.join(root_dir,m) for m in f90src],
-                      extra_compile_args=mycargs+myargs,
-                      extra_f90_compile_args=myfargs+myargs,
-                      libraries=mylib,
-                      extra_link_args=['-fopenmp']+mylink)
-extlist = [] if platform.system() == 'Windows' else [pyhamsExt]
-
-setup(
-    name='pyHAMS',
-    version='1.0.0',
-    description='Python module wrapping around HAMS',
-    author='NREL WISDEM Team',
-    author_email='systems.engineering@nrel.gov',
-    license='Apache License, Version 2.0',
-    package_data={'pyhams': []},
-    packages=['pyhams'],
-    ext_modules=extlist,
-)
+#myargs = ['-O3','-m64','-fPIC','-g']
+#mycargs = ['-std=c11']
+#if intel_flag:
+#    myfargs = ['-mkl']
+#    mylib = ['mkl_rt']
+#    mylink = []
+#else:
+#    myfargs = ['-fno-align-commons','-fdec-math']
+#    mylib = ['lapack']
+#    mylink = []
 
 
+#pyhamsExt = Extension('pyhams.libhams', sources=[os.path.join(root_dir,m) for m in f90src],
+#                      extra_compile_args=mycargs+myargs,
+#                      extra_f90_compile_args=myfargs+myargs,
+#                      libraries=mylib,
+#                      extra_link_args=['-fopenmp']+mylink)
+#extlist = [] if platform.system() == 'Windows' else [pyhamsExt]
