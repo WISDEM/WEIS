559f7837e5642fdb75f2d5eee627d82cd92c25ad
dzalkind <dzalkind@nrel.gov>
2024-01-12 13:49:13 -0700

    Tidy up 15_ example inputs

--- examples/15_RAFT_Studies/weis_driver_level3.py
+++ examples/15_RAFT_Studies/weis_driver_level3.py
@@ -6,8 +6,8 @@ import os, time, sys
 ## File management
 run_dir                = os.path.dirname( os.path.realpath(__file__) ) + os.sep
 wisdem_examples        = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
-fname_wt_input         = os.path.join(wisdem_examples,'09_floating/IEA-15-240-RWT_VolturnUS-S.yaml')
-# fname_wt_input         = '/Users/dzalkind/Tools/WEIS-4/examples/15_RAFT_Studies/IEA-15-floating.yaml'
+# fname_wt_input         = os.path.join(wisdem_examples,'09_floating/IEA-15-240-RWT_VolturnUS-S.yaml')
+fname_wt_input         = os.path.join(run_dir,'raft_opt_out.yaml')
 fname_modeling_options = run_dir + "modeling_options_level3.yaml"
 fname_analysis_options = run_dir + "analysis_options_level3.yaml"
 
