f9d4cef6441395c3103cb600d1618d39650a6703
Cory Frontin <cory.frontin@nrel.gov>
2024-06-17 14:50:47 -0600

    remove run script

--- examples/17_IEA22_Optimization/run.sh
+++
@@ -1,9 +0,0 @@
-
-CASE_NAME=SLSQP
-
-timeout 90m python -u driver_weis_raft_opt.py > >(tee stdout.log) 2> >(tee stderr.log >&2)
-
-mv stdout.log stdout_${CASE_NAME}.log
-mv stderr.log stderr_${CASE_NAME}.log
-rm -rf 32_DesignRound1_${CASE_NAME}
-mv 32_DesignRound1 32_DesignRound1_${CASE_NAME}
