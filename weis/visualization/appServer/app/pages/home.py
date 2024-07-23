871ce42d217afa06bde6c579ecb312feb670fc09
sryu <Sora.Ryu@nrel.gov>
2024-07-22 23:46:26 -0600

    add text field for reloading diff yaml file

--- weis/visualization/appServer/app/pages/home.py
+++ weis/visualization/appServer/app/pages/home.py
@@ -15,10 +15,13 @@ def layout():
 
     layout = dbc.Row([
                 dbc.InputGroup([
+                    dbc.Input(id='vizInput_path', placeholder='Enter input visualization file path..', type='text'),
+                    dbc.Button('Reload', id='reload', n_clicks=0)
+                    ], style={'width':'50vw', 'marginLeft': 50, 'marginTop': 50, 'display':'flex', 'justify-content':'space-between'}),
+                html.Div([
                     html.H3('vizInputFile'),
-                    dbc.Button('Reload VizInputFile', id='reload', n_clicks=0)
-                ], style={'width':'35vw', 'marginLeft': 50, 'marginTop': 50, 'display':'flex', 'justify-content':'space-between'}),
-                dbc.Col(dcc.Loading(html.Div(id='input-cfg-div')))
+                    dbc.Col(dcc.Loading(html.Div(id='input-cfg-div')))
+                    ], style={'width':'50vw', 'marginLeft': 50, 'marginTop': 50})
     ])
 
     return layout
@@ -33,20 +36,28 @@ def check_input_file(contents):
     '''
     if contents is None:
         raise PreventUpdate
-    
+
     if contents == {}:
         return html.Div([html.H5("Empty content..")])
-    
+
     file_tree_list = dict_to_html(contents, [], level=1)
-    
+
     return html.Div([*file_tree_list], style={'width':'80vw', 'marginLeft': 100, 'border-left-style':'dotted'})
 
 
 @callback(Output('input-dict', 'data'),
           State('input-dict', 'data'),
+          Input('vizInput_path', 'value'),
           Input('reload', 'n_clicks'))
-def reload_input_file(contents, btn):
-    updated_contents = parse_yaml(contents['yamlPath'])
+def reload_input_file(contents, vizInput_path, btn):
+
+    # Default
+    if vizInput_path is None:
+        updated_contents = parse_yaml(contents['yamlPath'])
+
+    # Update yaml file
+    if vizInput_path is not None and btn > 0:
+        contents['yamlPath'] = vizInput_path
+        updated_contents = parse_yaml(contents['yamlPath'])
 
     return updated_contents
-
