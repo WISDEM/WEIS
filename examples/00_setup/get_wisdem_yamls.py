299eac7b3c9697a457ab57c327bd81f3ebd342ea
Garrett Barter <garrett.barter@nrel.gov>
2025-01-04 20:45:08 -0700

    refresh to new directory structure

--- examples/00_setup/get_wisdem_yamls.py
+++ examples/00_setup/get_wisdem_yamls.py
@@ -1,29 +1,31 @@
 import os
 import requests
-""'
+"""
 Run this script to update geometry yamls based on WISDEM yamls
-""'
+"""
 
 def main():
 
-    wisdem_weis_map = {
-        # relative to examples/
-        # WISDEM example input                WEIS example
-        "09_floating/nrel5mw-spar_oc3.yaml":            "03_NREL5MW_OC3_spar/nrel5mw-spar_oc3.yaml",
-        "09_floating/nrel5mw-semi_oc4.yaml":            "04_NREL5MW_OC4_semi/nrel5mw-semi_oc4.yaml",
-        "09_floating/IEA-15-240-RWT_VolturnUS-S.yaml":  "06_IEA-15-240-RWT/IEA-15-240-RWT_VolturnUS-S.yaml",
-        "02_reference_turbines/IEA-15-240-RWT.yaml":    "06_IEA-15-240-RWT/IEA-15-240-RWT_Monopile.yaml",
-        "02_reference_turbines/IEA-3p4-130-RWT.yaml":   "05_IEA-3.4-130-RWT/IEA-3p4-130-RWT.yaml",
-        "03_blade/BAR_USC.yaml":   "08_OLAF/BAR_USC.yaml",
-    }
+    wisdem_ref_turbs = [
+        "09_floating/nrel5mw-spar_oc3.yaml",
+        "09_floating/nrel5mw-semi_oc4.yaml",
+        "09_floating/IEA-15-240-RWT_VolturnUS-S.yaml",
+        "09_floating/IEA-22-280-RWT_Floater.yaml",
+        "02_reference_turbines/IEA-10-198-RWT.yaml",
+        "02_reference_turbines/IEA-15-240-RWT.yaml",
+        "02_reference_turbines/IEA-22-280-RWT.yaml",
+        "02_reference_turbines/IEA-3p4-130-RWT.yaml",
+        "03_blade/BAR_USC.yaml",
+    ]
 
-    example_dir = os.path.join(os.path.dirname(__file__),"..")
+    setup_dir = os.path.dirname(__file__)
 
-    for wisdem_ex, weis_ex in wisdem_weis_map.items():
+    for wisdem_ex in wisdem_ref_turbs:
 
         raw_url = f"https://raw.githubusercontent.com/WISDEM/WISDEM/master/examples/{wisdem_ex}"
 
-        local_path = os.path.join(example_dir,weis_ex)
+        fname = wisdem_ex.split('/')[-1]
+        local_path = os.path.join(setup_dir, "ref_turbines", fname)
 
         # Download the file
         response = requests.get(raw_url)
