d6d8e82f656071aa5bad18bb050f49dc91b2d7a7
sryu <Sora.Ryu@nrel.gov>
2025-01-05 22:06:07 -0700

    check dash app creation

---
+++ weis/visualization/appServer/app/tests/test_app.py
@@ -0,0 +1,14 @@
+from contextvars import copy_context
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
+import dash
+
+# Import all of the names of callback functions to tests
+from mainApp import app
+
+def test_app_creation():
+    args = ["--input", "", "--host", "0.0.0.0"]
+    assert isinstance(app, dash.Dash)
+
+def test_layout_components():
+    assert app.layout is not None
