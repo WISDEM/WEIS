29f21c21fbc688841c84227f8c8db36d662d6814
dzalkind <dzalkind@nrel.gov>
2025-01-30 15:55:04 -0700

    Use previously generated .ss* inputs for linearization, update AD inputs

--- weis/aeroelasticse/LinearFAST.py
+++ weis/aeroelasticse/LinearFAST.py
@@ -125,7 +125,8 @@ class LinearFAST(runFAST_pywrapper_batch):
         case_inputs[("InflowWind","HWindSpeed")] = {'vals':self.wind_speeds, 'group':1}  # modelling input
 
         # AeroDyn Inputs
-        case_inputs[("AeroDyn","AFAeroMod")] = {'vals':[1], 'group':0}
+        case_inputs[("AeroDyn","DBEMT_Mod")] = {'vals':[-1], 'group':0}
+        case_inputs[("AeroDyn","UA_Mod")] = {'vals':[0], 'group':0}
 
         # Servodyn Inputs
         case_inputs[("ServoDyn","PCMode")] = {'vals':[0], 'group':0}
