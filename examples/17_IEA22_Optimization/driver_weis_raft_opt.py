cd12895efa83762256a36becaf392e473fae0dd4
dzalkind <dzalkind@nrel.gov>
2025-03-21 09:47:54 -0600

    Set example inputs to true runs, but TEST_MODE flag enabled

--- examples/17_IEA22_Optimization/driver_weis_raft_opt.py
+++ examples/17_IEA22_Optimization/driver_weis_raft_opt.py
@@ -2,6 +2,8 @@
 import os
 from weis import weis_main
 
+TEST_RUN = True
+
 ## File management
 run_dir = os.path.dirname( os.path.realpath(__file__) )
 fname_wt_input = os.path.join(run_dir, "..", "00_setup", "ref_turbines", "IEA-22-280-RWT_Floater.yaml")
@@ -9,7 +11,9 @@ fname_modeling_options = os.path.join(run_dir, "modeling_options_raft.yaml")
 fname_analysis_options = os.path.join(run_dir, "analysis_options_raft_ptfm_opt.yaml")
 
 wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
-                                                  fname_modeling_options, 
-                                                  fname_analysis_options)
+                                                 fname_modeling_options, 
+                                                 fname_analysis_options,
+                                                 test_run=TEST_RUN
+                                                 )
 
 
