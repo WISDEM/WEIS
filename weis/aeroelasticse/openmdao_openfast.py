8d9231f3864451e61012cf2953633a4bb2e44fc0
dzalkind <dzalkind@nrel.gov>
2025-04-08 15:12:08 -0600

    Use value of dlc/msw index series

--- weis/aeroelasticse/openmdao_openfast.py
+++ weis/aeroelasticse/openmdao_openfast.py
@@ -2770,7 +2770,7 @@ class FASTLoadCases(ExplicitComponent):
 
             # Filter for only stats of this dlc
             dlc_ind = cm['DLC'] == dlc
-            ss_dlc = sum_stats[dlc_ind]
+            ss_dlc = sum_stats[dlc_ind.values]
             cm_dlc = cm[dlc_ind]
 
             # Init this dlc char_load dict
@@ -2788,7 +2788,7 @@ class FASTLoadCases(ExplicitComponent):
                 # Over each wind speed simulated
                 for i, mws in enumerate(unique_mws):
                     mws_ind = cm_dlc[('InflowWind', 'HWindSpeed')] == mws
-                    ss_mws = ss_dlc[mws_ind]
+                    ss_mws = ss_dlc[mws_ind.values]
             
                     # average maximums (absolute value), most cases
                     char_loads[dlc][chan]['load_values'][i] = ss_mws[chan]['abs'].mean()
