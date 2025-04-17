1dc7cf0673aaf9c9d4fd2f97e8e0acdb8f1a7cc3
dzalkind <dzalkind@nrel.gov>
2025-04-17 15:39:55 -0600

    Move examples into 02

--- examples/02_generate_openfast_model_for_dlcs/weis_driver_oc3.py
+++ examples/02_generate_openfast_model_for_dlcs/weis_driver_oc3.py
@@ -6,8 +6,8 @@ TEST_RUN = True
 ## File management
 run_dir = os.path.dirname( os.path.realpath(__file__) )
 fname_wt_input = os.path.join(run_dir, "..", "00_setup", "ref_turbines", "nrel5mw-spar_oc3.yaml")
-fname_modeling_options = os.path.join(run_dir, "modeling_options.yaml")
-fname_analysis_options = os.path.join(run_dir, "analysis_options.yaml")
+fname_modeling_options = os.path.join(run_dir, "modeling_options_oc3.yaml")
+fname_analysis_options = os.path.join(run_dir, "analysis_options_oc3.yaml")
 
 wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
                                                  fname_modeling_options, 
---
+++ examples/05_control_codesign/weis_driver_rosco_opt.py
@@ -0,0 +1,16 @@
+import os
+from weis import weis_main
+
+TEST_RUN = True
+
+## File management
+run_dir = os.path.dirname( os.path.realpath(__file__) )
+fname_wt_input = os.path.join(run_dir, "..", "00_setup", "ref_turbines", "IEA-15-240-RWT.yaml")
+fname_modeling_options = os.path.join(run_dir, "modeling_options_rosco_opt.yaml")
+fname_analysis_options = os.path.join(run_dir, "analysis_options_rosco_opt.yaml")
+
+wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
+                                                  fname_modeling_options, 
+                                                  fname_analysis_options,
+                                                  test_run=TEST_RUN
+                                                  )
