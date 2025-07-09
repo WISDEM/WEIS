26712da0293c2989011c21baeb84b1a0260d1c58
dzalkind <dzalkind@nrel.gov>
2025-07-09 12:18:30 -0600

    Make test_BEM into a unit test, update paths

--- weis/frequency/test_BEM.py
+++ weis/frequency/test_BEM.py
@@ -1,33 +1,53 @@
 import os
 from weis import weis_main
+import unittest
 
-## File management
-run_dir = os.path.dirname( os.path.realpath(__file__) )
-fname_wt_input = os.path.join(run_dir, "..", "..", "examples", "00_setup", "ref_turbines", "IEA-15-240-RWT_VolturnUS-S_rectangular.yaml")
-fname_modeling_options = os.path.join(run_dir, "..", "..", "examples", "06_IEA-15-240-RWT", "modeling_options_umaine_semi.yaml")
-fname_analysis_options = os.path.join(run_dir, "..", "..", "examples", "06_IEA-15-240-RWT", "analysis_options.yaml")
 
-# override the modeling options so that not creating a new yaml file
-modeling_override = {}
-modeling_override["OpenFAST"] = {}
-modeling_override["OpenFAST"]["flag"] = False
-modeling_override["RAFT"] = {}
-modeling_override["RAFT"]["flag"] = True
-modeling_override["RAFT"]["potential_model_override"] = 0
-modeling_override["RAFT"]["potential_bem_members"] = ["main_column", "column1", "column2", "column3", "Y_pontoon_lower1", "Y_pontoon_lower2", "Y_pontoon_lower3"]
-modeling_override["RAFT"]["intersection_mesh"] = 1
-modeling_override["RAFT"]["characteristic_length_max"] = 1
-modeling_override["RAFT"]["characteristic_length_max"] = 3
-modeling_override["RAFT"]["plot_designs"] = True
-modeling_override["RAFT"]["save_designs"] = True
+class TestFrequency(unittest.TestCase):
 
-analysis_override = {}
-analysis_override["general"] = {}
-analysis_override["general"]["folder_output"] = run_dir
-                          
+    # @unittest.skipUnless("RUN_EXHAUSTIVE" in os.environ, "exhaustive on pull request only")
+    def test_BEM(self):
+        ## File management
+        run_dir = os.path.dirname( os.path.realpath(__file__) )
+        fname_wt_input = os.path.join(run_dir, "..", "..", "examples", "00_setup", "ref_turbines", "IEA-15-240-RWT_VolturnUS-S_rectangular.yaml")
+        fname_modeling_options = os.path.join(run_dir, "..", "..", "examples", "02_generate_openfast_model_for_dlcs" , "iea15_semi_modeling.yaml")
+        fname_analysis_options = os.path.join(run_dir, "..", "..", "examples", "02_generate_openfast_model_for_dlcs", "iea15_semi_analysis.yaml")
 
-wt_opt, modeling_options, opt_options = weis_main(fname_wt_input,
-                                                  fname_modeling_options,
-                                                  fname_analysis_options,
-                                                  modeling_override=modeling_override,
-                                                  analysis_override=analysis_override)
+        # override the modeling options so that not creating a new yaml file
+        modeling_override = {}
+        modeling_override["OpenFAST"] = {}
+        modeling_override["OpenFAST"]["flag"] = False
+        modeling_override["RAFT"] = {}
+        modeling_override["RAFT"]["flag"] = True
+        modeling_override["RAFT"]["potential_model_override"] = 0
+        modeling_override["RAFT"]["potential_bem_members"] = ["main_column", "column1", "column2", "column3", "Y_pontoon_lower1", "Y_pontoon_lower2", "Y_pontoon_lower3"]
+        modeling_override["RAFT"]["intersection_mesh"] = 1
+        modeling_override["RAFT"]["characteristic_length_max"] = 1
+        modeling_override["RAFT"]["characteristic_length_max"] = 3
+        modeling_override["RAFT"]["plot_designs"] = True
+        modeling_override["RAFT"]["save_designs"] = True
+
+        analysis_override = {}
+        analysis_override["general"] = {}
+        analysis_override["general"]["folder_output"] = run_dir
+                                
+
+        wt_opt, modeling_options, opt_options = weis_main(fname_wt_input,
+                                                        fname_modeling_options,
+                                                        fname_analysis_options,
+                                                        modeling_override=modeling_override,
+                                                        analysis_override=analysis_override,
+                                                        test_run=True
+                                                        )
+    
+
+
+if __name__ == "__main__":
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestFrequency)
+    result = unittest.TextTestRunner().run(suite)
+
+    if result.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
+
